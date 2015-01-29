8e936d176fb9da03561bedffa19cf44ffa2e4b19
Leonid Usachov <wirtoo@gmail.com>
2015-01-29 21:13:57 +0200

    fixtures added

--- src/Wizardry/MainBundle/DataFixtures/MongoDB/LoadCardData.php
+++ src/Wizardry/MainBundle/DataFixtures/MongoDB/LoadCardData.php
@@ -2,14 +2,25 @@
 
 namespace Wizardry\MainBundle\DataFixtures\MongoDB;
 
+use Hautelook\AliceBundle\Alice\DataFixtureLoader;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Wizardry\MainBundle\Document\Card;
 
-class LoadCardData extends FixtureInterface
+class LoadCardData extends DataFixtureLoader
 {
     public function load(ObjectManager $manager)
     {
 
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getFixtures()
+    {
+        return  array(
+            __DIR__.'/fixtures.yml',
+        );
     }
 }
