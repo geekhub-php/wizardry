b0892b4f33c711ebf245d9d83d8afb55d5d043b3
Leonid Usachov <wirtoo@gmail.com>
2015-01-29 21:21:13 +0200

    fixtures fixed

--- src/Wizardry/MainBundle/DataFixtures/MongoDB/LoadCardData.php
+++ src/Wizardry/MainBundle/DataFixtures/MongoDB/LoadCardData.php
@@ -3,16 +3,10 @@
 namespace Wizardry\MainBundle\DataFixtures\MongoDB;
 
 use Hautelook\AliceBundle\Alice\DataFixtureLoader;
-use Doctrine\Common\DataFixtures\FixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
-use Wizardry\MainBundle\Document\Card;
+use Nelmio\Alice\Fixtures;
 
 class LoadCardData extends DataFixtureLoader
 {
-    public function load(ObjectManager $manager)
-    {
-
-    }
 
     /**
      * {@inheritDoc}
