42e73abaa01becc09ea025f70f3ab762ea59e72a
Yura <ypalii2012@gmail.com>
2015-01-29 20:55:59 +0200

    added alice bundle

---
+++ src/Wizardry/MainBundle/DataFixtures/MongoDB/LoadCardData.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Wizardry\MainBundle\DataFixtures\MongoDB;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Wizardry\MainBundle\Document\Card;
+
+class LoadCardData extends FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+
+    }
+}
