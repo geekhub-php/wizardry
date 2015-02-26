8a08d85172211dfb8e7748e271cb3bdd3eedd679
Yura <ypalii2012@gmail.com>
2015-02-26 22:13:17 +0200

    declared Registration form

---
+++ src/Wizardry/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Wizardry\UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationFormType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstName');
+        $builder->add('lastName');
+    }
+
+    public function getParent()
+    {
+        return 'fos_user_registration';
+    }
+
+    public function getName()
+    {
+        return 'wizardry_user_registration';
+    }
+}
